ffbca1c5765e46e1643b429a8e5b5a97437b7501
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-08-28 20:22:42 +0100

    Implement basic HTTP server with /healthcheck endpoint

--- main.go
+++ main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"log"
+	"net/http"
+	"os"
 
-    // Third-party libraries
-    "github.com/joho/godotenv"
+	// Third-party libraries
+	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    // Load env vars from .env file
-    godotenv.Load(".env")
+	// Load env vars from .env file
+	godotenv.Load(".env")
 
-    portString := os.Getenv("PORT")
-    if portString == "" {
-        log.Fatal("Environment variable PORT is not set")
-    }
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Environment variable PORT is not set")
+	}
 
-    fmt.Println("PORT =", portString)
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET"},
+	}))
+
+	v1router := chi.NewRouter()
+	v1router.HandleFunc("/healthcheck", healthCheck)
+	router.Mount("/v1", v1router)
+
+	log.Printf("Starting an HTTP server on port %v", portString)
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
