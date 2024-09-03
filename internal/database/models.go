f56c32d85c0f43b7522b0cbec186fcc18984caca
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-09-03 21:07:57 +0100

    Formatting improvements

--- internal/database/models.go
+++ internal/database/models.go
@@ -11,5 +11,5 @@ type UserModel struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
-	ApiKey	  string	`json:"api_key"`
+	ApiKey    string    `json:"api_key"`
 }
