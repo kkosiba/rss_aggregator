63ed01a44733804f8dee6309a9ce79820378deb3
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-09-18 22:06:48 +0100

    Add FeedModel

--- internal/database/models.go
+++ internal/database/models.go
@@ -13,3 +13,12 @@ type UserModel struct {
 	Name      string    `json:"name"`
 	ApiKey    string    `json:"api_key"`
 }
+
+type FeedModel struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string    `json:"url"`
+	UserId    uuid.UUID `json:"user_id"`
+}
